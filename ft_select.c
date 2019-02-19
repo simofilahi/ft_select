1fa6466e09be3549c72de2f9d964a01ca9a16776
simofilahi <mohamedfilahi222@gmail.com>
2019-02-19 17:40:41 +0100

    adding ft_select

--- ft_select.c
+++ ft_select.c
@@ -1,6 +1,4 @@
 #include "inc/ft_select.h"
-#include <stdio.h>
-
 
 void create_node(t_output **head_ref, char *string)
 {
@@ -43,12 +41,69 @@ void print_list(t_output **head_ref)
      }
 }
 
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+
+void ft_select()
+{
+    struct termios config;
+    int ch;
+ 
+   if (!isatty(STDIN_FILENO))
+        ft_putendl("error file descriptor not pointing to a tty");
+    if (tcgetattr(STDIN_FILENO, &config))
+        ft_putendl("error can't get the current configuration");
+    config.c_lflag &= ~(ECHO | ICANON);
+    config.c_cc[VMIN]  = 1;
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &config) < 0)
+        ft_putendl("error can't apply the configuration");
+    while (1)
+    {
+        ch = 0;
+        if (read(STDIN_FILENO, &ch, 4))
+        {
+            if(ch == keyup)
+                ft_putendl("value of keyup is working");
+            else if(ch == keydown)
+                ft_putendl("value of keydown is working");
+            else if(ch == keyright)
+                ft_putendl("value of keyright is working");
+            else if(ch == keyleft)
+                ft_putendl("value of keyleft is working");
+            else if(ch == esc)
+            {
+                ft_putendl("value of esc is working");
+                break ;
+            }
+            else if(ch == delete)
+                ft_putendl("value of delete is working");
+            else if(ch == backspace)
+                ft_putendl("value of backspace is working");
+            else if(ch == space)
+                ft_putendl("value of space is working");
+            }
+        }
+        
+    }
+
 int main(int argc, char **argv)
 {
-    t_output **head;
+   t_output **head;
 
     (void)argc;
+    (void)argv;
     head = create_list(argv);
     print_list(head);
+    ft_select();
     return (0);
 }
--- inc/ft_select.h
+++ inc/ft_select.h
@@ -2,7 +2,20 @@
 #define FT_SELECT_H
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <termios.h>
 #include "../libft/libft.h"
+
+#define keyup 4283163
+#define keydown 4348699
+#define keyright 4414235
+#define keyleft 4479771
+#define esc 27
+#define delete 2117294875
+#define backspace 127
+#define space 32
+#define STDIN_FILENO 0
 
 typedef struct s_output
 {
